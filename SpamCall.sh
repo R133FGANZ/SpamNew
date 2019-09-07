81aa180041bb9983075ccdca9d5df407f09c3d00
R133FGANZ <50979010+SCTcyber@users.noreply.github.com>
2019-09-07 23:19:29 +0700

    Add files via upload

---
+++ SpamCall.sh
@@ -0,0 +1,91 @@
+load(){
+clear
+sleep 0.02
+echo "\033[1;32mLoading...(10%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(15%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(20%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(25%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(30%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(35%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(40%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(45%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(50%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(55%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(60%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(65%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(70%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(75%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(80%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(85%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(90%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(95%)"
+sleep 0.02
+clear
+echo "\033[1;32mLoading...(100%)"
+sleep 0.02
+clear
+}
+
+mulai(){
+clear
+echo "\033[1;34m"
+figlet "SpamCall"
+echo "\033[1;32m"
+echo "##########################################"
+echo "#                                        #"
+echo "# Author : Mr.4R13F                      #"
+echo "# Team   : DevlinCyberTeam               #"
+echo "#                                        #"
+echo "##########################################"
+echo "\033[1;31mTools SpamCall"
+echo "\033[1;37mketikan y untuk lanjut"
+read -p "Lanjutkan [y/n] :" Lanjut
+
+if [ $Lanjut = y ]; then
+clear
+echo "\033[1;31m"
+figlet "spam"
+echo "\033[1;32m"
+php 1.php
+fi
+}
+
+load
+load
+python2 please.py
+clear
+mulai
